3cad2deb10c33fff339b7eb95654d970597be4cf
anaego <anastas.ego@gmail.com>
2021-08-14 22:36:28 +0300

    Fixed grid centering

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,20 +9,20 @@ public class Grid : MonoBehaviour
     public PuzzleNode[] nodes;
     public NodeConnection[] connections;
     public RectTransform gridBackground;
-    [SerializeField]
-    private Transform startTransform;
-
+    //public Transform startTransform;
 
     public int rowNumber = 3;
     public int columnNumber = 3;
 
-    private float xOffset = 1;
-    private float yOffset = 1;
+    private float xOffset = 5;
+    private float yOffset = 5;
+    private float mainOffset = 2.5f;
 
     public void Awake()
     {
-        //xOffset = (columnNumber + 1);
-        //yOffset = (rowNumber + 1);
+        // TODO Since now we're instantiating everything from the middle, offsets need to change sign?
+        xOffset = xOffset / (columnNumber + 1);
+        yOffset = yOffset / (rowNumber + 1);
     }
 
     public int CountConnectedNodes(PuzzleNode node)
@@ -37,7 +37,7 @@ public class Grid : MonoBehaviour
 
     public void PlaceNodes()
     {
-        Vector2 offset = startTransform.localPosition;
+        //Vector2 offset = startTransform.localPosition;
         for (int column = 1; column <= columnNumber; column++)
         {
             for (int row = 1; row <= rowNumber; row++)
@@ -45,16 +45,44 @@ public class Grid : MonoBehaviour
                 var node = nodes.FirstOrDefault(puzzleNode => puzzleNode.positionInGridX == row && puzzleNode.positionInGridY == column);
                 if (node != null)
                 {
-                    // nodes are instantiated in the upper left corner
                     var instnode = Instantiate(NodePrefab, this.transform, false);
-                    var nodeScale = instnode.transform.localScale.x;
-                    instnode.transform.position = offset + new Vector2(
-                        ((xOffset + nodeScale) * column) ,
-                        ((yOffset + nodeScale) * row)
-                    );
+                    //var nodeScale = instnode.transform.localScale.x;
+                    //instnode.transform.position = offset + new Vector2(
+                    //    ((xOffset + nodeScale) * column) ,
+                    //    ((yOffset + nodeScale) * row)
+                    //);
+                    instnode.transform.localPosition = new Vector3(
+                        instnode.transform.localPosition.x
+                            + xOffset * column
+                            - mainOffset
+                            //- instnode.GetComponent<RectTransform>().rect.width * 0.5f
+                            ,
+                        instnode.transform.localPosition.y
+                            - yOffset * row
+                            + mainOffset
+                            //+ instnode.GetComponent<RectTransform>().rect.width * 0.5f
+                            ,
+                        instnode.transform.localPosition.z);
                 }
             }
         }
+
+        //for (int column = 1; column <= columnNumber; column++)
+        //{
+        //    for (int row = 1; row <= rowNumber; row++)
+        //    {
+        //        var node = nodes.FirstOrDefault(puzzleNode => puzzleNode.positionInGridX == row && puzzleNode.positionInGridY == column);
+        //        if (node != null)
+        //        {
+        //            // nodes are instantiated in the upper left corner
+        //            var instnode = Instantiate(NodePrefab, this.transform, false);
+        //            instnode.transform.localPosition = new Vector3(
+        //                instnode.transform.localPosition.x + xOffset * column - instnode.GetComponent<RectTransform>().rect.width * 0.5f,
+        //                instnode.transform.localPosition.y - yOffset * row + instnode.GetComponent<RectTransform>().rect.width * 0.5f,
+        //                instnode.transform.localPosition.z);
+        //        }
+        //    }
+        //}
     }
 
     public bool CheckPuzzleSolved()
