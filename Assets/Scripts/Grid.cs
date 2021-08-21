10cc73550089a1fa871d102039f32dd966cc1bc6
anaego <anastas.ego@gmail.com>
2021-08-21 18:28:32 +0300

    Finished turning ui elements into sprites

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -14,15 +14,19 @@ public class Grid : MonoBehaviour
     public int rowNumber = 3;
     public int columnNumber = 3;
 
-    private float xOffset = 5;
-    private float yOffset = 5;
-    private float mainOffset = 2.5f;
+    private float xOffset = 10;
+    private float yOffset = 10;
+    private float mainOffset = 5f;
+    private float nodeWidth = 1.5f;
 
     public void Awake()
     {
         // TODO Since now we're instantiating everything from the middle, offsets need to change sign?
         xOffset = xOffset / (columnNumber + 1);
         yOffset = yOffset / (rowNumber + 1);
+        mainOffset = xOffset * 2;
+        // Presuming that node is 1 unit in length
+        nodeWidth = NodePrefab.gameObject.transform.localScale.x;
     }
 
     public int CountConnectedNodes(PuzzleNode node)
@@ -46,43 +50,17 @@ public class Grid : MonoBehaviour
                 if (node != null)
                 {
                     var instnode = Instantiate(NodePrefab, this.transform, false);
-                    //var nodeScale = instnode.transform.localScale.x;
-                    //instnode.transform.position = offset + new Vector2(
-                    //    ((xOffset + nodeScale) * column) ,
-                    //    ((yOffset + nodeScale) * row)
-                    //);
                     instnode.transform.localPosition = new Vector3(
                         instnode.transform.localPosition.x
                             + xOffset * column
-                            - mainOffset
-                            //- instnode.GetComponent<RectTransform>().rect.width * 0.5f
-                            ,
+                            - mainOffset,
                         instnode.transform.localPosition.y
                             - yOffset * row
-                            + mainOffset
-                            //+ instnode.GetComponent<RectTransform>().rect.width * 0.5f
-                            ,
+                            + mainOffset,
                         instnode.transform.localPosition.z);
                 }
             }
         }
-
-        //for (int column = 1; column <= columnNumber; column++)
-        //{
-        //    for (int row = 1; row <= rowNumber; row++)
-        //    {
-        //        var node = nodes.FirstOrDefault(puzzleNode => puzzleNode.positionInGridX == row && puzzleNode.positionInGridY == column);
-        //        if (node != null)
-        //        {
-        //            // nodes are instantiated in the upper left corner
-        //            var instnode = Instantiate(NodePrefab, this.transform, false);
-        //            instnode.transform.localPosition = new Vector3(
-        //                instnode.transform.localPosition.x + xOffset * column - instnode.GetComponent<RectTransform>().rect.width * 0.5f,
-        //                instnode.transform.localPosition.y - yOffset * row + instnode.GetComponent<RectTransform>().rect.width * 0.5f,
-        //                instnode.transform.localPosition.z);
-        //        }
-        //    }
-        //}
     }
 
     public bool CheckPuzzleSolved()
