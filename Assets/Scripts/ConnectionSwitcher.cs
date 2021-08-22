c4e1c3202c9e1bd03644c58994e4f539980d78f7
anaego <anastas.ego@gmail.com>
2021-08-22 20:04:01 +0300

    Started working on connections between nodes

---
+++ Assets/Scripts/ConnectionSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionSwitcher : MonoBehaviour
+{
+    public NodeConnection connection1;
+    public NodeConnection connection2;
+
+    public void OnMouseUp()
+    {
+        if (!connection1.gameObject.activeInHierarchy && !connection1.gameObject.activeInHierarchy)
+        {
+            connection1.gameObject.SetActive(true);
+        }
+    }
+}
